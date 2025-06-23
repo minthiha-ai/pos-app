835aa7709a31b4c9d2ee05e5b697cb7a348f001f
MinThiha <51397647+minthiha-ai@users.noreply.github.com>
2025-06-23 18:39:53 +0700

    first commit

---
+++ resources/pos/src/config/environment.js
@@ -0,0 +1,7 @@
+export const environment = {
+    URL:
+        window.location.protocol +
+        "//" +
+        window.location.hostname +
+        (window.location.port ? ":" + window.location.port : ""),
+};
